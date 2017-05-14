a205a984a042b5dd3ca13c96d4cd92451bcbce1d
heli <heli@sogoodapp.com>
2017-05-15 00:54:46 +0800

    my co

--- myco/co.js
+++ myco/co.js
@@ -1,18 +1,17 @@
-//exports.run = 
-export function co(generator) {
-    var gen = generator();
-    function next(data) {
-        var ret = gen.next(data);
-        if (ret.done) {
-            return;
-        } else {
-            ret.value(function cb(err, data) {
-                if (err) {
-                    throw (err);
-                }
-                next(data);
-            });
+exports.run = function (generator) {
+  var gen = generator();
+  function next(data) {
+    var ret = gen.next(data);
+    if (ret.done) {
+      return;
+    } else {
+      ret.value(function cb(err, data) {
+        if (err) {
+          throw (err);
         }
+        next(data);
+      });
     }
-    next();
+  }
+  next();
 }
